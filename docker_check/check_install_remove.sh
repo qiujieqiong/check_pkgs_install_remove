426c0062fe1a31ae743e1c0069a2329f3fbbb975
qiujieqiong <qiujieqiong@linuxdeepin.com>
2017-09-05 13:49:45 +0800

    fix depends err before remove

--- docker_check/check_install_remove.sh
+++ docker_check/check_install_remove.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get -y install python3-pip dbus
 LC_CTYPE="en_US.UTF-8" pip3 install --trusted-host pypi.douban.com -i http://pypi.douban.com/simple/ pandas
-cd /docker_check_pkgs_install_remove/check_pkgs_install_remove/docker_check
+cd /docker_check_pkgs_install_remove/docker_check
 #python3 get_sourcelist.py
 #mv /etc/apt/sources.list /etc/apt/sources.list.bak
 #mv base.list /etc/apt/sources.list.d/
